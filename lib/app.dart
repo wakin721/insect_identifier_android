8470c8294ebdc41ef96d593d0e09808ee3178c5e
和錦わきん <156275155+wakin721@users.noreply.github.com>
2026-07-27 20:10:10 +0800

    connect appearance settings to app theme

--- lib/app.dart
+++ lib/app.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'controllers/app_controller.dart';
+import 'controllers/appearance_controller.dart';
 import 'core/app_theme.dart';
 import 'screens/home_shell.dart';
 
 class InsectIdentifierApp extends StatefulWidget {
   const InsectIdentifierApp({
     required this.controller,
+    required this.appearanceController,
     super.key,
   });
 
   final AppController controller;
+  final AppearanceController appearanceController;
 
   @override
   State<InsectIdentifierApp> createState() => _InsectIdentifierAppState();
@@ -20,24 +23,28 @@ class InsectIdentifierApp extends StatefulWidget {
 class _InsectIdentifierAppState extends State<InsectIdentifierApp> {
   @override
   void dispose() {
+    widget.appearanceController.dispose();
     widget.controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '虫鉴',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      darkTheme: AppTheme.dark(),
-      themeMode: ThemeMode.system,
-      locale: const Locale('zh', 'CN'),
-      supportedLocales: const <Locale>[
-        Locale('zh', 'CN'),
-      ],
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
-      home: HomeShell(controller: widget.controller),
+    return ListenableBuilder(
+      listenable: widget.appearanceController,
+      builder: (context, _) {
+        return MaterialApp(
+          title: '虫鉴',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.light(widget.appearanceController.seedColor),
+          darkTheme: AppTheme.dark(widget.appearanceController.seedColor),
+          themeMode: widget.appearanceController.themeMode,
+          locale: const Locale('zh', 'CN'),
+          supportedLocales: const <Locale>[Locale('zh', 'CN')],
+          localizationsDelegates: GlobalMaterialLocalizations.delegates,
+          home: HomeShell(controller: widget.controller),
+        );
+      },
     );
   }
 }
